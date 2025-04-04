14fdd9cf8b3063585f8c2e08c20355b0b2bdf5bf
tsunakit99 <tainosuke0926@gmail.com>
2025-04-04 18:58:09 +0900

    add: infra層にリポジトリを実装

---
+++ backend/infra/prisma-task.repository.ts
@@ -0,0 +1,12 @@
+import { Task } from "@model/task";
+import { PrismaClient } from "@prisma/client";
+import { TaskRepository } from "@repo/task.repository";
+
+const prisma = new PrismaClient();
+
+export class PrismaTaskRepository implements TaskRepository {
+    async create(title: string): Promise<Task> {
+        const task = await prisma.task.create({ data: { title } });
+        return task;
+    }
+}
