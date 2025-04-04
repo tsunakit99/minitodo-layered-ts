20670eefe0670d46ef375f5dce2d7348797d91f0
tsunakit99 <tainosuke0926@gmail.com>
2025-04-04 20:09:46 +0900

    add: Task作成サービスの実装

---
+++ backend/service/task.service.ts
@@ -0,0 +1,11 @@
+import { Task } from "@model/task";
+import { TaskRepository } from "@repo/task.repository";
+
+export class TaskService {
+    constructor(private readonly repo: TaskRepository) { }
+    
+    async create(title: string): Promise<Task> { 
+        if (!title || title.trim() === "") throw new Error("Title is required");
+        return this.repo.create(title);
+    }
+}
