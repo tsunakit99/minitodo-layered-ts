7587f5ff782f32d3ce8d672080c737f32d407a58
tsunakit99 <tainosuke0926@gmail.com>
2025-04-05 16:16:48 +0900

    add: findbyId, update, deleteのインターフェース定義

--- backend/repository/task.repository.ts
+++ backend/repository/task.repository.ts
@@ -3,4 +3,7 @@ import { Task } from "@model/task";
 export interface TaskRepository {
     create(title: string): Promise<Task>;
     findAll(): Promise<Task[]>;
+    findById(id: string): Promise<Task | null>;
+    update(id: string, task: Partial<Task>): Promise<Task>;
+    delete(id: string): Promise<void>;
 }
