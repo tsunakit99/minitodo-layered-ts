5d805cd6f2d46db1d6f68499f1c92e82d2ab5cfd
tsunakit99 <tainosuke0926@gmail.com>
2025-04-05 16:19:58 +0900

    add: ハンドラ実装

--- backend/controller/task.controller.ts
+++ backend/controller/task.controller.ts
@@ -24,3 +24,27 @@ export async function GET() {
         return NextResponse.json({ error: error.message }, { status: 400 });
     }
 }
+
+export async function PATCH(request: NextRequest) {
+    try {
+        const { id, title } = await request.json();
+        const repo = new PrismaTaskRepository();
+        const service = new TaskService(repo);
+        const task = await service.updateTask(id, title);
+        return NextResponse.json(task, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    try {
+        const { id } = await request.json();
+        const repo = new PrismaTaskRepository();
+        const service = new TaskService(repo);
+        await service.deleteTask(id);
+        return NextResponse.json({ message: "Task deleted" }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+}
